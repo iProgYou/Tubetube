50e514bdc0fcebae42e2c54af26a8ac46ead6b91
Zack <zack.musicteacher@gamil.com>
2020-04-30 20:13:14 -0700

    Setup Video index

---
+++ app/controllers/api/videos_controller.rb
@@ -0,0 +1,23 @@
+class Api::VideosController < ApplicationController
+
+    def index
+        @videos = Video.all
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+end
--- app/models/video.rb
+++ app/models/video.rb
@@ -16,6 +16,7 @@ class Video < ApplicationRecord
         foreign_key: :creator_id,
         class_name: "User"
 
+        
     has_one_attached :video
 
 end
