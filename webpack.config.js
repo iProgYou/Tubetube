55970f213a1719333f6f5010bebc6668764c3d28
Zack <zack.musicteacher@gamil.com>
2020-06-05 12:18:16 -0700

    fix video upload css

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,9 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             importLoaders: 1,
-                            modules: true
+                            modules: {
+                                localIdentName: '[name]__[local]--[hash:base64:5]'
+                            }
                         }
                     }
                 ],
