a83eaa8d7710a4b05c69b17e0deb126624b87cdc
Zack <zack.musicteacher@gamil.com>
2020-06-15 10:49:51 -0700

    fix Datetime on search page

--- frontend/components/search/search_page_item.jsx
+++ frontend/components/search/search_page_item.jsx
@@ -3,6 +3,17 @@ import styles from './search_page.module.css';
 import { Link } from 'react-router-dom';
 
 const SearchPageItem = ({ video }) => {
+
+    const parseDate = () => {
+        if(!video.createdAt) return null;
+        console.log(video)
+        let date = video.createdAt.slice(0,10).split('-')
+        const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+        let currentMonth = month[parseInt(date[1]) - 1]
+        let currentDay = parseInt(date[2]).toString()
+        let newDate = `${currentMonth} ${currentDay}, ${date[0]}`
+        return newDate
+    }
     
     // console.log(video);
     // Clickin on creator should eventually link to page
@@ -24,7 +35,7 @@ const SearchPageItem = ({ video }) => {
                             <div>•</div>
                             <div>{video.plays} views</div>
                             <div>•</div>
-                            <div>{video.createdAt}</div>
+                            <div>{parseDate()}</div>
                         </div>
                         <div className={styles.description}>{video.description}</div>
                     </div>
